cf7cecf6d1ae57ac30454bbaaa2c3cc10b58b1c0
Abdullokh Alimov <66109159+Alimov-8@users.noreply.github.com>
2021-02-05 19:28:31 +0500

    Update views.py

--- IUTDeadlinesProject/IUTDeadlines/views.py
+++ IUTDeadlinesProject/IUTDeadlines/views.py
@@ -1,24 +1,96 @@
 from django.shortcuts import render, redirect
-from .models import SophomoreDeadlines, SophomoreSubjects
+from .models import SophomoreDeadlines, SophomoreSubjects, FreshmanSubjects, SeniorSubjects, JuniorSubjects
 
+
+# Sophomores page
 def index(request):
-    Days = SophomoreDeadlines.objects.order_by('id') 
-    Subjects = SophomoreSubjects.objects.order_by('day')
+    Days = SophomoreDeadlines.objects.order_by('id')
+    Subjects = SophomoreSubjects.objects.order_by('year')
     emptyDays=[]
     for d in Days:
         isDayFull = False
         for s in Subjects:
-            if s.day.day == d.day:
+            if s.year.year == d.year:
                 isDayFull = True
         if isDayFull == False:
-            emptyDays.append(d.day)
+            emptyDays.append(d.year)
         else:
-            emptyDays.append(1) 
+            emptyDays.append(1)
 
     allDays = zip(Days, emptyDays)
-                 
-    
+
+
     context = {'Subjects' : Subjects, 'allDays': allDays}
     return render(request, 'IUTDeadlines/index.html', context)
+
+
+# Freshman page
+def freshman(request):
+    Days = SophomoreDeadlines.objects.order_by('id')
+    Subjects = FreshmanSubjects.objects.order_by('year')
+    emptyDays=[]
+    for d in Days:
+        isDayFull = False
+        for s in Subjects:
+            if s.year.year == d.year:
+                isDayFull = True
+        if isDayFull == False:
+            emptyDays.append(d.year)
+        else:
+            emptyDays.append(1)
+
+    allDays = zip(Days, emptyDays)
+
+
+    context = {'Subjects' : Subjects, 'allDays': allDays}
+    return render(request, 'IUTDeadlines/freshman.html', context)
+
+
+# Junior page
+def junior(request):
+    Days = SophomoreDeadlines.objects.order_by('id')
+    Subjects = SeniorSubjects.objects.order_by('year')
+    emptyDays=[]
+    for d in Days:
+        isDayFull = False
+        for s in Subjects:
+            if s.year.year == d.year:
+                isDayFull = True
+        if isDayFull == False:
+            emptyDays.append(d.year)
+        else:
+            emptyDays.append(1)
+
+    allDays = zip(Days, emptyDays)
+
+
+    context = {'Subjects' : Subjects, 'allDays': allDays}
+    return render(request, 'IUTDeadlines/junior.html', context)
+
+
+# Senior page
+def senior(request):
+    Days = SophomoreDeadlines.objects.order_by('id')
+    Subjects = SeniorSubjects.objects.order_by('year')
+    emptyDays=[]
+    for d in Days:
+        isDayFull = False
+        for s in Subjects:
+            if s.year.year == d.year:
+                isDayFull = True
+        if isDayFull == False:
+            emptyDays.append(d.year)
+        else:
+            emptyDays.append(1)
+
+    allDays = zip(Days, emptyDays)
+
+
+    context = {'Subjects' : Subjects, 'allDays': allDays}
+    return render(request, 'IUTDeadlines/senior.html', context)
+
+
+
+
 
 
