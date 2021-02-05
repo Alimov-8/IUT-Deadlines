19be48226414f00c5e1460685accd6491870c6f6
Abdullokh Alimov <66109159+Alimov-8@users.noreply.github.com>
2021-02-05 19:26:21 +0500

    Update models.py

--- IUTDeadlinesProject/IUTDeadlines/models.py
+++ IUTDeadlinesProject/IUTDeadlines/models.py
@@ -6,7 +6,7 @@ class SophomoreDeadlines(models.Model):
     year = models.DateTimeField() # 12.12.2020
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return (self.day + str("  ") + str(self.year))
 
@@ -14,8 +14,35 @@ class SophomoreSubjects(models.Model):
     subject = models.CharField(max_length=50) #IP
     deadline = models.CharField(max_length=50) #12:00
     location = models.CharField(max_length=50) # eclass
-    day = models.ForeignKey(SophomoreDeadlines, on_delete=models.CASCADE,) # Choose day
-    
+    year = models.ForeignKey(SophomoreDeadlines, on_delete=models.CASCADE,) # Choose day
+
+    def __str__(self):
+        return self.subject
+
+class FreshmanSubjects(models.Model):
+    subject = models.CharField(max_length=50) #IP
+    deadline = models.CharField(max_length=50) #12:00
+    location = models.CharField(max_length=50) # eclass
+    year = models.ForeignKey(SophomoreDeadlines, on_delete=models.CASCADE,) # Choose day
+
+    def __str__(self):
+        return self.subject
+
+class SeniorSubjects(models.Model):
+    subject = models.CharField(max_length=50) #IP
+    deadline = models.CharField(max_length=50) #12:00
+    location = models.CharField(max_length=50) # eclass
+    year = models.ForeignKey(SophomoreDeadlines, on_delete=models.CASCADE,) # Choose day
+
+    def __str__(self):
+        return self.subject
+
+class JuniorSubjects(models.Model):
+    subject = models.CharField(max_length=50) #IP
+    deadline = models.CharField(max_length=50) #12:00
+    location = models.CharField(max_length=50) # eclass
+    year = models.ForeignKey(SophomoreDeadlines, on_delete=models.CASCADE,) # Choose day
+
     def __str__(self):
         return self.subject
 
