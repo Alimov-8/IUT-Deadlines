19af75a9db23a7c96aa25034eabaa829749fc346
Abdullokh Alimov <66109159+Alimov-8@users.noreply.github.com>
2021-02-05 19:27:14 +0500

    Update urls.py

--- IUTDeadlinesProject/IUTDeadlines/urls.py
+++ IUTDeadlinesProject/IUTDeadlines/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('freshman', views.freshman, name='freshman'),
+    path('junior', views.junior, name='junior'),
+    path('senior', views.senior, name='senior')
 ]
